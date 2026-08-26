5414158cbd5540370bbb47b19351a5a57da9723d
曾國瑜 <>
2026-08-26 17:01:44 +0800

    Codex Auto Publish

--- games/sudoku/game.js
+++ games/sudoku/game.js
@@ -1,1 +1,6 @@
-window.WPClassicLogic?.mount("sudoku");
+(function () {
+  "use strict";
+  window.WPClassicLogic?.mount("sudoku");
+  const poster = document.querySelector(".logic-poster img");
+  if (poster) poster.src = "../../assets/sudoku-cover-v1.webp";
+})();
--- src/site-config.js
+++ src/site-config.js
@@ -1,5 +1,5 @@
 window.WONDER_SITE = {
-  version: "v0.29.72",
+  version: "v0.29.73",
   localization: {
     defaultLocale: "en",
     fallbackLocale: "en",
